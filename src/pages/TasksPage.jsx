0d2c543a4dc10ebe466d667cc640d6df81fae46d
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-10-10 21:57:32 +0200

    Update to project

--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Helmet } from 'react-helmet';
-import { TaskList } from 'components/TaskList/TaskList';
-import { TaskEditor } from 'components/TaskEditor/TaskEditor';
+import { TaskList } from '../components/TaskList';
+import { TaskEditor } from '../components/TaskEditor';
 import { fetchTasks } from 'redux/tasks/operations';
 import { selectLoading } from 'redux/tasks/selectors';
 
